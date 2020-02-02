d44eb9d9e352f44d7382f04fea4d71eb93f8bb59
unknown <ravikumarmahalingam@yahoo.com>
2020-02-02 17:56:25 +0530

    Serialization comments added

--- src/com/paypal/Serialization/SerializationExample.java
+++ src/com/paypal/Serialization/SerializationExample.java
@@ -5,18 +5,22 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+//First run the program to generate the serialization file
+//Next uncomment the 5 files for deserialization
+//If you change the value of the serialVersionUID during 
+//Deserialization then you will get classCastException. 
 public class SerializationExample {
 	public static void main(String[] args) throws Exception {
 		
-//		FileOutputStream fileOutputStream = new FileOutputStream("Ravi.Ser");
-//		ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
-//		out.writeObject(new Serial(1, "Ravi"));
+		FileOutputStream fileOutputStream = new FileOutputStream("Ravi.Ser");
+		ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
+		out.writeObject(new Serial(1, "Ravi"));
 		
-		FileInputStream fileInputStream = new FileInputStream("Ravi.Ser");
-		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-		Serial serial = (Serial)objectInputStream.readObject();
-		
-		System.out.println("Value of the id:::::" + serial.getId());
-		System.out.println("Value of the name:::" + serial.getName());
+//		FileInputStream fileInputStream = new FileInputStream("Ravi.Ser");
+//		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//		Serial serial = (Serial)objectInputStream.readObject();
+//		
+//		System.out.println("Value of the id:::::" + serial.getId());
+//		System.out.println("Value of the name:::" + serial.getName());
 	}
 }
